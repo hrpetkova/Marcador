086e22b1f1c830ac3cceb02da9caf29820bec169
Hristiyana Velichkova Petkova <hr.petkova@alumnos.upm.es>
2025-07-20 19:04:39 +0200

    Update types.ts

--- models/types.ts
+++ models/types.ts
@@ -1,1 +1,55 @@
-..
+// models/types.ts
+
+export type Player = {
+  number: number;
+  name: string;
+  isGoalkeeper: boolean;
+  goals: number;
+  yellow: boolean;
+  twoMinutes: number;
+  red: boolean;
+  blue: boolean;
+};
+
+export type Staff = {
+  name: string;
+  role: string;
+  yellow: boolean;
+  twoMinutes: number;
+  red: boolean;
+};
+
+export type Team = {
+  name: string;
+  shirtColor: string;
+  shortsColor: string;
+  players: Player[];
+  staff: Staff[];
+  isSelection: boolean;
+};
+
+export type Category = {
+  name: string;
+  isSelection: boolean;
+  gender?: "M" | "F";
+  halves: number;
+  halfDuration: number;
+  restDuration: number;
+  allowsOvertime: boolean;
+  allowsPenalties: boolean;
+  phases: string[];
+};
+
+export type GoalEvent = {
+  playerNumber: number;
+  team: "A" | "B";
+  time: string;
+};
+
+export type MatchData = {
+  teamA: Team;
+  teamB: Team;
+  category: Category;
+  goals: GoalEvent[];
+};
+
