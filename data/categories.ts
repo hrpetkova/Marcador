d8a44e65b6f14df109e88f7c23c4639cb7a0e8e1
Hristiyana Velichkova Petkova <hr.petkova@alumnos.upm.es>
2025-07-20 19:05:01 +0200

    Update categories.ts

--- data/categories.ts
+++ data/categories.ts
@@ -1,1 +1,38 @@
-..
+// data/categories.ts
+
+import { Category } from "../models/types";
+
+export const categories: Category[] = [
+  {
+    name: "Alevín Masculino",
+    isSelection: false,
+    gender: "M",
+    halves: 2,
+    halfDuration: 20,
+    restDuration: 10,
+    allowsOvertime: false,
+    allowsPenalties: false,
+    phases: ["Amistoso", "Liga", "Copa"]
+  },
+  {
+    name: "2ª Nacional Masculina",
+    isSelection: false,
+    halves: 2,
+    halfDuration: 30,
+    restDuration: 10,
+    allowsOvertime: true,
+    allowsPenalties: true,
+    phases: ["Amistoso", "Liga", "Copa", "Fase Ascenso", "Final A4"]
+  },
+  {
+    name: "ASOBAL",
+    isSelection: false,
+    halves: 2,
+    halfDuration: 30,
+    restDuration: 10,
+    allowsOvertime: true,
+    allowsPenalties: true,
+    phases: ["Amistoso", "Liga", "Copa del Rey", "EHF"]
+  },
+  // Añadir el resto siguiendo esta estructura
+];
