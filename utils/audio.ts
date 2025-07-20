19be133dc49ec08b13047daa0d40be98b9dbfdb5
Hristiyana Velichkova Petkova <hr.petkova@alumnos.upm.es>
2025-07-20 19:05:29 +0200

    Update audio.ts

--- utils/audio.ts
+++ utils/audio.ts
@@ -1,1 +1,10 @@
-..
+// utils/audio.ts
+
+import { Audio } from 'expo-av';
+
+export const playClaxon = async () => {
+  const { sound } = await Audio.Sound.createAsync(
+    require('../assets/claxon.mp3')
+  );
+  await sound.playAsync();
+};
